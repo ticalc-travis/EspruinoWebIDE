ee64a4cf30b02962eb02123d3260d4bb12385147
Travis Evans <travisgevans@gmail.com>
2023-08-29 15:59:58 -0500

    test: Does this fix `codeMirror` undefined error when changing editor settings?

--- js/core/editorJavaScript.js
+++ js/core/editorJavaScript.js
@@ -41,10 +41,10 @@
       type : { "default": "default", "3024-day": "3024-day", "3024-night": "3024-night", "abcdef": "abcdef", "ambiance": "ambiance", "ayu-dark": "ayu-dark", "ayu-mirage": "ayu-mirage", "base16-dark": "base16-dark", "base16-light": "base16-light", "bespin": "bespin", "blackboard": "blackboard", "cobalt": "cobalt", "colorforth": "colorforth", "darcula": "darcula", "dracula": "dracula", "duotone-dark": "duotone-dark", "duotone-light": "duotone-light", "eclipse": "eclipse", "elegant": "elegant", "espruino": "espruino", "erlang-dark": "erlang-dark", "gruvbox-dark": "gruvbox-dark", "hopscotch": "hopscotch", "icecoder": "icecoder", "idea": "idea", "isotope": "isotope", "lesser-dark": "lesser-dark", "liquibyte": "liquibyte", "lucario": "lucario", "material": "material", "material-darker": "material-darker", "material-palenight": "material-palenight", "material-ocean": "material-ocean", "mbo": "mbo", "mdn-like": "mdn-like", "midnight": "midnight", "monokai": "monokai", "moxer": "moxer", "neat": "neat", "neo": "neo", "night": "night", "nord": "nord", "oceanic-next": "oceanic-next", "panda-syntax": "panda-syntax", "paraiso-dark": "paraiso-dark", "paraiso-light": "paraiso-light", "pastel-on-dark": "pastel-on-dark", "railscasts": "railscasts", "rubyblue": "rubyblue", "seti": "seti", "shadowfox": "shadowfox", "solarized dark": "solarized dark", "solarized light": "solarized light", "the-matrix": "the-matrix", "tomorrow-night-bright": "tomorrow-night-bright", "tomorrow-night-eighties": "tomorrow-night-eighties", "ttcn": "ttcn", "twilight": "twilight", "vibrant-ink": "vibrant-ink", "xq-dark": "xq-dark", "xq-light": "xq-light", "yeti": "yeti", "yonce": "yonce", "zenburn": "zenburn" },
       defaultValue : "default",
       onChange : function(newValue) {
-        if (codeMirror) {
-          loadThemeCSS(Espruino.Config.THEME);
-          codeMirror.setOption('theme', Espruino.Config.THEME);
-        }
+	loadThemeCSS(Espruino.Config.THEME);
+	for (editor of Espruino.Core.EditorJavaScript.getEditors()) {
+	  editor.codeMirror.setOption('theme', Espruino.Config.THEME);
+	}
       }
     });
     Espruino.Core.Config.add("INDENTATION_TYPE", {
